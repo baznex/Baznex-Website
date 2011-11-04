50d3c461166c99bb1c9bc133cb1e36ebbe3203b9
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-03 20:31:53 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject baznex-website "0.1.0-SNAPSHOT"
+            :description "Website for self-directed community software organization BAZNEX"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.0"]]
+            :main baznex-website.server)
+
---
+++ src/baznex_website/views/welcome.clj
@@ -0,0 +1,10 @@
+(ns baznex-website.views.welcome
+  (:use [baznex-website.views.common :only (layout)]
+        [noir.core :only [defpage]]
+        hiccup.core
+        hiccup.page-helpers))
+
+(defpage "/welcome" []
+  (layout
+    [:h1 "B.A.Z.N.E.X."]
+    [:h2 "Boston Autonomous Zone Nerd Nexus"]))
