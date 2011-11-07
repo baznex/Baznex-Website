bd849bd8c7720f163d0fc0f00862bcc51bed00e9
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-06 21:28:52 -0500

    killed silly macro

--- src/baznex_website/views/common.clj
+++ src/baznex_website/views/common.clj
@@ -4,17 +4,12 @@
         hiccup.core
         hiccup.page-helpers))
 
-(defmacro defpartial5 [fname params & body]
-  `(defpartial ~fname ~params
-     (html5
-       ~@body)))
-
-(defpartial5 baznex-layout [title & content]
-  [:head
-    [:title (str "B.A.Z.N.E.X. - " title)]
-      (map include-css ["/css/reset.css"])
+(defpartial baznex-layout [title & content]
+  (html5
+    [:head
+      [:title (str "B.A.Z.N.E.X. - " title)]
+      (include-css "/css/reset.css")
       [:style {:type "text/css"} (baznex-css)] ]
     [:body
-    [:header]
-    [:div#wrapper content]
-    [:footer]])
+      [:div#wrapper content]
+      [:footer]] ))
