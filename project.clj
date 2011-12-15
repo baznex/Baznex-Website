a79a77deebf75f52c2ac7665d1950e53551002a1
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-14 20:13:45 -0500

    added When and Where to index page

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
             :description "Website for self-directed community software organization BAZNEX"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.0"]
-                           [cssgen "0.2.5-SNAPSHOT"]]
+                           [org.clojars.daaku/cssgen "0.2.5-SNAPSHOT"]]
             :main baznex-website.server)
 
--- src/baznex_website/views/index.clj
+++ src/baznex_website/views/index.clj
@@ -4,9 +4,19 @@
         hiccup.core
         hiccup.page-helpers))
 
-(defpage "/" []
-  (baznex-layout "Boston Autonomous Zone Nerd Nexus"
-    [:h1 "What's New?"]
-    [:ul
-      [:li [:a {:href "/manifesto"} "BAZNEX Manifesto"]]
-      [:li [:a {:href "http://groups.google.com/group/baznex" :target "_blank"} "Baznex Google Group"]]]))
+(let [google-group-link [:a {:href "http://groups.google.com/group/baznex" :target "_blank"} "BAZNEX Google Group"]
+      sprout-link [:a {:href "http://thesprouts.org/" :target "_blank"} "Sprout in Davis Square"]]
+
+  (defpage "/" []
+    (baznex-layout "Boston Autonomous Zone Nerd Nexus"
+      [:h1 "What"]
+      [:ul
+        [:li [:a {:href "/manifesto"} "BAZNEX Manifesto"]]
+        [:li [:a {:href "http://groups.google.com/group/baznex" :target "_blank"} "Baznex Google Group"]]]
+      
+      [:h1 "Where"]
+      [:p "Currently gathering at " sprout-link] 
+    
+      [:h1 "When"] 
+      [:p "Every other Wednesday at 6pm ending whenever you want to leave, but you can't sleep here."]
+      [:p "Check the " google-group-link " for the next date."] )))
