db27538c18d8156d14c8859f0b8bf5353a7da496
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-16 21:39:33 -0500

    added some periods

--- src/baznex_website/views/name.clj
+++ src/baznex_website/views/name.clj
@@ -10,7 +10,7 @@
     [:h3 "It is an accurate yet intentionally goofy name, because we are serious about having a good time."] 
 
     [:h2 "Boston"]
-    [:p "We're based out of Boston"]
+    [:p "We're based out of Boston."]
 
     [:h2 "Autonomous Zone"]
     [:blockquote "In the formation of a Temporary Autonomous Zone, Bey argues, information becomes a key tool that sneaks into the cracks of formal procedures. A new territory of the moment is created that is on the boundary line of established regions. Any attempt at permanence that goes beyond the moment deteriorates to a structured system that inevitably stifles individual creativity. It is this chance at creativity that is real empowerment."]
@@ -26,4 +26,4 @@
     [:p "As we all connect together we generate good vibes, and new exciting ideas are born."]
     
     [:h2 "Honorable Mention"]
-    [:p "It begins with 'Baz' the third most important computer science word behind 'Foo' and 'Bar'"]))
+    [:p "It begins with 'Baz' the third most important computer science word behind 'Foo' and 'Bar'."]))
