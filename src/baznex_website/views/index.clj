e6980bcd6489e36190b7a6874267b2062648ff60
Alex Baranosky <alexander.baranosky@gmail.com>
2012-02-05 15:13:36 -0500

    tweaked opening statement of index page.

--- src/baznex_website/views/index.clj
+++ src/baznex_website/views/index.clj
@@ -7,7 +7,7 @@
 (defpage "/" []
   (baznex-layout "Boston Autonomous Zone Nerd Nexus"
     [:h1 "What"]
-    [:h3 [:em "An UnMeetup: "] "We’re a self-directed group of programmers focused on the joys of software development, community, and learning. We want to work together in small groups to contribute and learn as we go. The openspace conference of the meetup world."]
+    [:h3 [:em "An UnMeetup: "] "The openspace conference of the meetup world. We’re a self-directed group of programmers focused on the joys of software development, community, and learning. We want to work together in small groups to contribute and learn as we go."]
   
     [:ul
       [:li manifesto-link]
